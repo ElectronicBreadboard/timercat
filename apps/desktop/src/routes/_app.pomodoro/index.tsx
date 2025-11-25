b48d074a37b41fcfc087314fdb5e2abb8c91f39e
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-11-25 17:19:30 +0100

    #develop updated background color

--- apps/desktop/src/routes/_app.pomodoro/index.tsx
+++ apps/desktop/src/routes/_app.pomodoro/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/_app/pomodoro/')({
 function RouteComponent() {
 	return (
 		<div className="bg-base-150 h-screen flex-1 p-2">
-			<div className="relative h-full overflow-y-auto rounded-sm bg-white">Pomodoro Timer</div>
+			<div className="bg-base-50 relative h-full overflow-y-auto rounded-sm">Pomodoro Timer</div>
 		</div>
 	);
 }
--- apps/desktop/src/routes/_app.settings.general/index.tsx
+++ apps/desktop/src/routes/_app.settings.general/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/_app/settings/general/')({
 function RouteComponent() {
 	return (
 		<div className="bg-base-150 h-screen flex-1 py-2 pr-2 pl-1">
-			<div className="relative h-full overflow-y-auto rounded-sm bg-white">Settings</div>
+			<div className="bg-base-50 relative h-full overflow-y-auto rounded-sm">Settings</div>
 		</div>
 	);
 }
