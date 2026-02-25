efbed3f2a16524d4cffcad8f0d97141ecc891422
Benno <57860196+bennoinbeta@users.noreply.github.com>
2026-02-25 06:19:59 +0100

    #develop fix prettier ignore

--- apps/focuscat-web/.content-collections/generated/index.d.ts
+++ apps/focuscat-web/.content-collections/generated/index.d.ts
@@ -1,7 +1,7 @@
-import { GetTypeByName } from '@content-collections/core';
-import configuration from '../../content-collections.ts';
+import configuration from "../../content-collections.ts";
+import { GetTypeByName } from "@content-collections/core";
 
-export type PomodorocatBlog = GetTypeByName<typeof configuration, 'pomodorocatBlog'>;
+export type PomodorocatBlog = GetTypeByName<typeof configuration, "pomodorocatBlog">;
 export declare const allPomodorocatBlogs: Array<PomodorocatBlog>;
 
 export {};
